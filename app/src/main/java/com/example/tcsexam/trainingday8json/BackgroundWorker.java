0b16e78d8f79f4dc24b67f07608736c13dddc4fc
Vibhanshu Rai <vibhanshu09@outlook.com>
2018-04-09 15:29:35 +0530

    Localised.

--- app/src/main/java/com/example/tcsexam/trainingday8json/BackgroundWorker.java
+++ app/src/main/java/com/example/tcsexam/trainingday8json/BackgroundWorker.java
@@ -1,13 +1,17 @@
 package com.example.tcsexam.trainingday8json;
 
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.support.v7.app.AlertDialog;
+
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -38,9 +42,10 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String... strings) {
         String status = strings[0];
-        String loginurl = "http://www.projectclub.in/loginU.php";
-        String registrationurl = "http://www.projectclub.in/registerU.php";
-        if (status.equals("login")) {
+        String loginurl = "http://172.18.41.112/TrainingDay7/loginU.php";
+        String registrationurl = "http://172.18.41.112/TrainingDay7/registerU.php";
+
+        if (status.equals("login")){
             String username = strings[1];
             String password = strings[2];
             try {
@@ -50,12 +55,12 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
                 httpURLConnection.setDoInput(true);
                 httpURLConnection.setDoOutput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
 
-                String postData = URLEncoder.encode("username", "UTF-8") + "=" +
-                        URLEncoder.encode(username, "UTF-8") + "&" +
-                        URLEncoder.encode("password", "UTF-8") + "=" +
-                        URLEncoder.encode(password, "UTF-8");
+                String postData = URLEncoder.encode("username","UTF-8")+"="+
+                        URLEncoder.encode(username,"UTF-8")+"&"+
+                        URLEncoder.encode("password","UTF-8")+"="+
+                        URLEncoder.encode(password,"UTF-8");
 
                 bufferedWriter.write(postData);
                 bufferedWriter.flush();
@@ -64,10 +69,10 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
 
                 //For Getting server Responce
                 InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
                 String result = "";
                 String line;
-                while ((line = bufferedReader.readLine()) != null) {
+                while ((line=bufferedReader.readLine())!=null){
                     result += line;
                 }
                 bufferedReader.close();
@@ -79,7 +84,7 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
                 e.printStackTrace();
             }
         }
-        if (status.equals("registration")) {
+        if (status.equals("registration")){
             String name = strings[1];
             String mobile = strings[2];
             String email = strings[3];
@@ -92,17 +97,18 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                String postData = URLEncoder.encode("name", "UTF-8") + "=" +
-                        URLEncoder.encode(name, "UTF-8") + "&" +
-                        URLEncoder.encode("mobile", "UTF-8") + "=" +
-                        URLEncoder.encode(mobile, "UTF-8") + "&" +
-                        URLEncoder.encode("email", "UTF-8") + "=" +
-                        URLEncoder.encode(email, "UTF-8") + "&" +
-                        URLEncoder.encode("username", "UTF-8") + "=" +
-                        URLEncoder.encode(username, "UTF-8") + "&" +
-                        URLEncoder.encode("password", "UTF-8") + "=" +
-                        URLEncoder.encode(password, "UTF-8");
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String postData = URLEncoder.encode("name","UTF-8")+"="+
+                        URLEncoder.encode(name,"UTF-8")+"&"+
+                        URLEncoder.encode("mobile","UTF-8")+"="+
+                        URLEncoder.encode(mobile,"UTF-8")+"&"+
+                        URLEncoder.encode("email","UTF-8")+"="+
+                        URLEncoder.encode(email,"UTF-8")+"&"+
+                        URLEncoder.encode("username","UTF-8")+"="+
+                        URLEncoder.encode(username,"UTF-8")+"&"+
+                        URLEncoder.encode("password","UTF-8")+"="+
+                        URLEncoder.encode(password,"UTF-8");
+
                 bufferedWriter.write(postData);
                 bufferedWriter.flush();
                 bufferedWriter.close();
@@ -110,11 +116,11 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
 
                 //For Getting server Responce
                 InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
                 String result = "";
                 String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    result += line;
+                while ((line=bufferedReader.readLine())!=null){
+                    result +=line;
                 }
                 bufferedReader.close();
                 inputStream.close();
@@ -130,45 +136,42 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
 
     @Override
     protected void onPreExecute() {
-        super.onPreExecute();
         builder = new AlertDialog.Builder(activity);
+
         progressDialog = new ProgressDialog(context);
         progressDialog.setTitle("Please Wait");
         progressDialog.setMessage("Connecting to server...");
         progressDialog.setIndeterminate(true);
         progressDialog.setCancelable(false);
         progressDialog.show();
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
     }
 
     @Override
     protected void onPostExecute(String json) {
-        super.onPostExecute(json);
-        progressDialog.dismiss();
-        JSONObject jsonObject = null;
+        Toast.makeText(context, json, Toast.LENGTH_SHORT).show();
         try {
-            jsonObject = new JSONObject(json);
+            progressDialog.dismiss();
+            JSONObject jsonObject = new JSONObject(json);
             JSONArray jsonArray = jsonObject.getJSONArray("server_responce");
             JSONObject jsonObject1 = jsonArray.getJSONObject(0);
             String code = jsonObject1.getString("code");
             String message = jsonObject1.getString("message");
             if (code.equals("register_success") || code.equals("register_false"))
-                showDialog("Registration Status",message,code);
+               // Toast.makeText(context, code + " : " + message, Toast.LENGTH_SHORT).show();
+                showDialog("Registration Status",code,message);
             else if (code.equals("login_false"))
-                showDialog("Login Status",message,code);
+                //Toast.makeText(context, code + " : " + message, Toast.LENGTH_SHORT).show();
+                showDialog("Login Status",code,message);
             else if (code.equals("login_true")){
-                showDialog("Login Status",message,code);
+                //Toast.makeText(context, code + " : " + message, Toast.LENGTH_SHORT).show();
+                showDialog("Login Status",code,message);
             }
-
-        } catch (Exception e){
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
-    public void showDialog(String tital,String message, String code){
+    public void showDialog(String tital,String code, String message){
         builder.setTitle(tital);
         if (code.equals("register_success") || code.equals("register_false")){
             builder.setMessage(message);
@@ -177,8 +180,14 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
                 public void onClick(DialogInterface dialogInterface, int i) {
                     dialogInterface.dismiss();
                     activity.finish();
+                    Log.e("Error: ","1st");
                 }
             });
+            Toast.makeText(context, code + " : " + message, Toast.LENGTH_SHORT).show();
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+
         }
         else if (code.equals("login_false")){
             builder.setMessage(message);
@@ -186,13 +195,15 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     dialogInterface.dismiss();
+                    Log.e("Error: ","2nd");
                 }
             });
+            Toast.makeText(context, code + " : " + message, Toast.LENGTH_SHORT).show();
             AlertDialog alertDialog = builder.create();
             alertDialog.show();
+
         }
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
+
 
     }
 
--- app/src/main/java/com/example/tcsexam/trainingday8json/RegistrationActivity.java
+++ app/src/main/java/com/example/tcsexam/trainingday8json/RegistrationActivity.java
@@ -2,6 +2,7 @@ package com.example.tcsexam.trainingday8json;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,5 +15,65 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        registrationName = findViewById(R.id.registration_name);
+        registrationMobile = findViewById(R.id.registration_mobile);
+        registrationEmail = findViewById(R.id.registration_email);
+        registrationUsername = findViewById(R.id.registration_username);
+        registrationPassword = findViewById(R.id.registration_password);
+
+        register = findViewById(R.id.register_user_button);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getTextsFromEditText()){
+                    //TODO:  Registration here
+                    String status = "registration";
+                    BackgroundWorker backgroundWorker = new BackgroundWorker(RegistrationActivity.this);
+                    backgroundWorker.execute(status, name, mobile, email, username, password);
+                }
+            }
+        });
+
+    }
+
+    private boolean getTextsFromEditText(){
+        if (registrationName.getText().toString().trim().isEmpty()){
+            registrationName.setError("Enter Name");
+            return false;
+        }
+        else
+            name = registrationName.getText().toString().trim();
+
+        if (registrationMobile.getText().toString().trim().isEmpty()) {
+            registrationMobile.setError("Enter Mobile number");
+            return false;
+        }
+        else
+            mobile = registrationMobile.getText().toString().trim();
+
+        if (registrationEmail.getText().toString().trim().isEmpty()){
+            registrationName.setError("Enter Email");
+            return false;
+        }
+        else
+            email = registrationEmail.getText().toString().trim();
+
+        if (registrationUsername.getText().toString().trim().isEmpty()){
+            registrationName.setError("Enter Username");
+            return false;
+        }
+        else
+            username = registrationUsername.getText().toString().trim();
+
+        if (registrationPassword.getText().toString().trim().isEmpty()){
+            registrationName.setError("Enter Password");
+            return false;
+        }
+        else
+            password = registrationPassword.getText().toString().trim();
+
+        return true;
     }
 }
